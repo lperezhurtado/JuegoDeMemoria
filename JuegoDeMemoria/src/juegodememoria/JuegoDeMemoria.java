ef2693979876f6030d2ee50ffaa267e02a0a3c57
luis(clase) <lperezhurtado97@gmail.com>
2021-11-30 16:50:25 +0100

    clonado en clase

--- JuegoDeMemoria/src/juegodememoria/JuegoDeMemoria.java
+++ JuegoDeMemoria/src/juegodememoria/JuegoDeMemoria.java
@@ -7,6 +7,7 @@ public class JuegoDeMemoria {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Clonado en clase");
         
 
     }
